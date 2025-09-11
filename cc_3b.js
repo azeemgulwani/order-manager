3356e5f558d1277eca5be4270f6a193ee520dce9
Azeem Gulwani <azeemgulwani@usf.edu>
2025-09-10 20:27:24 -0400

    Added Inventory Array and Products

--- cc_3b.js
+++ cc_3b.js
@@ -1,2 +1,12 @@
 // Coding Challenge 3b
 
+// Step 2: Model the Inventory
+let inventory = [
+{sku: "SKU-001", name:"Eco Bottle", price:"19.99", stock:"27"},
+{sku: "SKU-002", name:"Bamboo Toothbrush", price:"4.49", stock:"20"},
+{sku: "SKU-003", name:"Reusable Straw Set", price:"9.99", stock:"18"},
+{sku: "SKU-004", name:"Cnavas Tote", price:"13.99", stock:"25"},
+];
+
+
+
